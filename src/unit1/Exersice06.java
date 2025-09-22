3c54797ec66f1972e72351d67f7afee4b6727aa7
mw.rodriguez <mw.rodriguez@106-24.info2>
2025-09-22 10:07:24 +0200

    Exersice 6 uploaded

---
+++ src/unit1/Exersice06.java
@@ -0,0 +1,35 @@
+package unit1;
+
+import java.util.Scanner;
+
+public class Exersice06 {
+
+	public static void main(String[] args) {
+
+		//Variables are declared
+		int num1;
+		int num2;
+		
+		//Scanner object is declared
+		Scanner scanner = new Scanner(System.in);
+		
+		//User is asked for the first numer
+		System.out.print("Introduce the first number: ");
+		num1 = scanner.nextInt();
+		
+		//User is asked for the second number
+		System.out.print("Introduce the second number: ");
+		num2 = scanner.nextInt();
+		
+		//All operations are printed
+		System.out.println("Addition: " + (num1 + num2));
+		System.out.println("Substraction: " + (num1 - num2));
+		System.out.println("Multiplication: " + (num1 * num2));
+		System.out.println("Division: " + (num1 / num2));
+		
+		//Scanner is closed
+		scanner.close();
+
+	}
+
+}
